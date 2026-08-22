0ee81c139be3e12d78e2ca68db3ad8f1501bd711
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-08-22 18:21:43 +0300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using System;
+
+namespace MODNAME
+{
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string MOD_GUID = "CREATOR.MODNAME";
+        public const string MOD_NAME = "MODNAME";
+        public const string MOD_VERSION = "0.0.0";
+
+        public void Awake()
+        {
+        }
+    }
+}
